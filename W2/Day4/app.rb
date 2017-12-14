59f89a275addd02bf2d4a9229bc01bfb44c9f009
LeeYunJeong0215 <yyj077643@gmail.com>
2017-12-14 14:11:49 +0900

    DAY4 second commit

--- W2/Day4/app.rb
+++ W2/Day4/app.rb
@@ -12,3 +12,15 @@ end
 get '/' do
   erb :index
 end
+
+#form
+get '/post' do
+  erb :post
+end
+
+#결과 보여주는 곳
+get '/complete' do
+  @title = params[:title]
+  @content = params[:content]
+  erb :complete
+end
